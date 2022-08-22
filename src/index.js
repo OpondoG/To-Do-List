afa1368beade0ef371e06127e6abea963d3187ba
Okonjo <okonjogilbert2@gmail.com>
2022-08-22 18:15:15 +0300

    to-do structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
 import './style.css';
+
+const toDoTasks = [{
+        description: 'Clean the house',
+        completed: false,
+        index: 1,
+    },
+    {
+        description: 'Cook food',
+        completed: false,
+        index: 2,
+    },
+    {
+        description: 'Do shopping',
+        completed: false,
+        index: 3,
+    },
+    {
+        description: 'Finish assignment',
+        completed: false,
+        index: 4,
+    },
+
+];
+
+toDoTasks.forEach((element) => {
+    const wholeList = document.querySelector('.tasks');
+    const taskList = document.createElement('div');
+    taskList.classList.add('choreDetails');
+    taskList.innerHTML = `<div class="oneChore">
+    <input type="checkbox">
+    <h2>${element.description}</h2>
+   </div>
+   <div class="icon">
+   <i class="fa-solid fa-ellipsis-vertical"></i>
+   </div>`;
+    wholeList.appendChild(taskList);
+});
